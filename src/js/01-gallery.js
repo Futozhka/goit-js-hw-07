1f31b007233b011263a3fe26424960433d6f731d
Futozhka <124589958+Futozhka@users.noreply.github.com>
2023-11-14 15:14:54 +0000

    start1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,37 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
 console.log(galleryItems);
+const galleryList = document.querySelector('.gallery');
+
+const createGalleryItemsMarkup = items => {
+  return items
+    .map(({ preview, original, description }) => `
+      <li class="gallery__item">
+        <a class="image__link" href="${original}">
+          <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" />
+        </a>
+      </li>
+    `)
+    .join('');
+};
+
+const markGallery = createGalleryItemsMarkup(galleryItems);
+galleryList.insertAdjacentHTML('beforeend', markGallery);
+
+// Обробник події для відкриття модального вікна
+galleryList.addEventListener('click', event => {
+  event.preventDefault();
+  
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  const largeImageURL = event.target.dataset.source;
+  console.log('Large Image URL:', largeImageURL);
+
+  const instance = basicLightbox.create(`
+    <img src="${largeImageURL}" width="800" height="600">
+  `);
+
+  instance.show();
+});
